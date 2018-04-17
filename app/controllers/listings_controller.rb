e254a460918d50e53e4fad2e4808e69b7e1057dc
Bruno Garcia Gonzalez <BrunoGarciaGonzalez@outlook.com>
2018-04-17 09:50:21 -0400

    added listings show view

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -2,11 +2,11 @@ class ListingsController < ApplicationController
 	before_action :set_listing, only: [:show, :edit, :update, :destroy]
 	#Create
 	def new
-		
+
 	end
 
 	def create
-		
+
 	end
 	#Read
 	def index
@@ -14,22 +14,22 @@ class ListingsController < ApplicationController
 	end
 
 	def show
-		
+		# populated via :set_listing
 	end
 
 	#Update
 
 	def edit
-		
+
 	end
 
 	def update
-		
+
 	end
 
 	#Destroy
 	def destroy
-		
+
 	end
 
 	private
